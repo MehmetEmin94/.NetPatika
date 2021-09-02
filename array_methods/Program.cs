1a6c4b5a60c1993595d743d5179b3287b16f8bcf
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-09-02 15:25:08 +0300

    02.09

---
+++ array_methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace array_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sorting
+            int[] numArr={24,12,4,88,3,11};
+            Array.Sort(numArr);
+
+            //Clear
+            Array.Clear(numArr,2,1);
+
+            //Reverse
+            Array.Reverse(numArr);
+
+            //IndexOf
+            Array.IndexOf(numArr,23);
+
+            //Resize
+            Array.Resize<int>(ref numArr,9)
+        }
+    }
+}
